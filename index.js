63e17806ee92f921194f45c8527a1b2f0c3ec1d3
Emil Repmann <emil.repmann@gmail.com>
2025-12-01 22:37:50 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const navbar = document.getElementsByTagName("nav")[0];
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
