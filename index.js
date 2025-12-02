afdea93f23b6db6b22a44139ff237d58613e90df
Emil Repmann <emil.repmann@gmail.com>
2025-12-02 18:51:05 +0100

    design done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const navbar = document.getElementsByTagName("nav")[0];
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 10) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
